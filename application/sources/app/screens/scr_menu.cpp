226217e206734491277c59a2549adb033bc6d56a
QuocBuu <pquocbuu@gmail.com>
2023-09-08 11:57:59 +0700

    update

--- application/sources/app/screens/scr_menu.cpp
+++ application/sources/app/screens/scr_menu.cpp
@@ -1,129 +1,98 @@
 #include "scr_menu.h"
-
+/*****************************************************************************/
+/* Instructions for adding new items to the menu*/
+/*****************************************************************************/
+/*  ** Để làm nhanh nhấn "Ctrl + F" để tìm theo từ khóa **
+	Bước 1: Chuẩn bị icon cho item sẽ thêm vào kích thước 13-18 px*px
+	Bước 2: Thêm 1 item vào menu
+			- Tăng số lượng item: tăng "NUMBER_MENU_ITEMS" thêm 1
+			- Thêm định danh: tăng "NUMBER_MENU_ITEMS" thêm 1 item
+			- Thêm định dạng màu: tăng "MENU_ITEMS_ICON_COLOR" thêm 1 item
+	Bước 5: Thêm tên cho item vào "menu_items_name[]"
+	Bước 6: Thêm icon cho item:
+			- Thêm icon: menu_items_icon[]
+			- Thêm kich thước: menu_items_icon_size_w[], menu_items_icon_size_h[]
+	Bước 7: Thêm địa chỉ cho item:
+			- Thêm 1 item vào: "screen_tran_menu()" rồi điền địa chỉ muốn 
+			chuyển tới.
+	## Nếu icon không cân xứng vào "menu_items_icon_axis_y[3]" để điều chỉnh lại.
+*/
 
 /*****************************************************************************/
 /* Variable and Struct Declaration - Menu game */
 /*****************************************************************************/
 // Screen 
 #define STEP_MENU_CHOSSE				(22)
-#define NUMBER_MENU_ITEMS				(11)
+#define NUMBER_MENU_ITEMS				(4)
 #define	SCREEN_MENU_H					(64)
-
-#define FRAMES_AXIS_Y() \
-do { \
-	frame_white.axis_y =frame[screen_menu.location-screen_menu.screen].axis_y; \
-	frame[0].axis_y = 0; \
-	frame[1].axis_y = 22; \
-	frame[2].axis_y = 44; \
-} while(0);
 
 #define MENU_ITEMS_ICON_COLOR() \
 do { \
-	menu_items_icon_color[0] = !menu_chosse.items.is_archery_game; \
-	menu_items_icon_color[1] = !menu_chosse.items.is_setting; \
-	menu_items_icon_color[2] = !menu_chosse.items.is_charts; \
-	menu_items_icon_color[3] = !menu_chosse.items.is_exit_0; \
-	menu_items_icon_color[4] = !menu_chosse.items.is_exit_1; \
-	menu_items_icon_color[5] = !menu_chosse.items.is_exit_2; \
-	menu_items_icon_color[6] = !menu_chosse.items.is_exit_3; \
-	menu_items_icon_color[7] = !menu_chosse.items.is_exit_4; \
-	menu_items_icon_color[8] = !menu_chosse.items.is_exit_5; \
-	menu_items_icon_color[9] = !menu_chosse.items.is_exit_6; \
-	menu_items_icon_color[10] = !menu_chosse.items.is_exit_7; \
+	menu_items_icon_color[0]	= !menu_chosse.items.is_item_1; \
+	menu_items_icon_color[1]	= !menu_chosse.items.is_item_2; \
+	menu_items_icon_color[2]	= !menu_chosse.items.is_item_3; \
+	menu_items_icon_color[3]	= !menu_chosse.items.is_item_4; \
 } while(0);
 
 struct menu_items{
 // menu items
-	unsigned int is_archery_game : 1;
-	unsigned int is_setting : 1;
-	unsigned int is_charts : 1;
-	unsigned int is_exit_0 : 1;
-	unsigned int is_exit_1 : 1;
-	unsigned int is_exit_2 : 1;
-	unsigned int is_exit_3 : 1;
-	unsigned int is_exit_4 : 1;
-	unsigned int is_exit_5 : 1;
-	unsigned int is_exit_6 : 1;
-	unsigned int is_exit_7 : 1;
-	unsigned int is_exit_9 : 1;
-	unsigned int is_exit_10 : 1;
-	unsigned int is_exit_11 : 1;
+	unsigned int is_item_1 : 1;
+	unsigned int is_item_2 : 1;
+	unsigned int is_item_3 : 1;
+	unsigned int is_item_4 : 1;
 };
+
+// Menu items name
+static char menu_items_name[NUMBER_MENU_ITEMS][20] = {
+	"   Archery Game1  ",		// item 1
+	"   Archery Game2  ",		// item 2
+	"   Archery Game3  ",		// item 3
+	"   Archery Game4  ",		// item 4
+};
+
 // Menu items icon
 static const uint8_t *menu_items_icon[NUMBER_MENU_ITEMS] = {
-	archery_icon, \
-	setting_icon, \
-	chart_icon, \
-	exit_icon, \
-	archery_icon, \
-	archery_icon, \
-	archery_icon, \
-	archery_icon, \
-	archery_icon, \
-	archery_icon, \
-	archery_icon, \
+	archery_icon,				// item 1
+	setting_icon,				// item 2
+	chart_icon,					// item 3
+	exit_icon,					// item 4
+};
+
+// Menu items size W
+uint8_t menu_items_icon_size_w[NUMBER_MENU_ITEMS] = {
+	15,							// item 1
+	16,							// item 2
+	16,							// item 3
+	15,							// item 4
+};
+
+uint8_t menu_items_icon_size_h[NUMBER_MENU_ITEMS] = {
+	15,							// item 1
+	16,							// item 2
+	16,							// item 3
+	15,							// item 4
 };
 
 // Menu items color
 uint8_t menu_items_icon_color[NUMBER_MENU_ITEMS];
-uint8_t menu_items_icon_size_w[NUMBER_MENU_ITEMS] = {
-	15, \
-	16, \
-	16, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-};
-uint8_t menu_items_icon_size_h[NUMBER_MENU_ITEMS] = {
-	15, \
-	16, \
-	16, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-	15, \
-};
-uint8_t menu_items_icon_axis_y[3] = {2, 24,	46};
-
-// Menu items name
-static char menu_items_name[NUMBER_MENU_ITEMS][20] = {
-	"   Archery Game1  ", 
-	"   Archery Game2  ", 
-	"   Archery Game3  ", 
-	"   Archery Game4  ",
-	"   Archery Game5  ",
-	"   Archery Game6  ",
-	"   Archery Game7  ",
-	"   Archery Game8  ",
-	"   Archery Game9  ",
-	"   Archery Game10 ",
-	"   Archery Game11 ",
+// Menu items axis Y
+uint8_t menu_items_icon_axis_y[3] = {
+	2,							// icon frame 1
+	24,							// icon frame 2
+	46							// icon frame 3
 };
 
+// Screen and item location on the Menu
 typedef struct {
 	int screen;
 	int location;
-} Screen;
+} screen_t;
 
-Screen screen_menu;
-
-// Menu items
-
+// Menu items ID
 union scr_menu_t {
 	uint32_t _id = 1;		// if items >> => uint8_t -> uint16_t ->uint32_t
 	menu_items items;
 };
-
-scr_menu_t menu_chosse;
 
 // Scroll bar
 typedef struct {
@@ -132,8 +101,6 @@ typedef struct {
 	uint8_t size_W = 3;
 	uint8_t size_h = SCREEN_MENU_H / NUMBER_MENU_ITEMS;
 } scr_menu_scroll_bar_t;
-
-scr_menu_scroll_bar_t scroll_bar;
 
 // Frames
 typedef struct {
@@ -144,6 +111,9 @@ typedef struct {
 	uint8_t size_r = 3;
 } scr_menu_frames_t;
 
+screen_t screen_menu;
+scr_menu_t menu_chosse;
+scr_menu_scroll_bar_t scroll_bar;
 scr_menu_frames_t frame_white;
 scr_menu_frames_t frame[3];
 
@@ -168,14 +138,8 @@ view_screen_t scr_menu = {
 };
 
 void view_scr_menu() {
-// ICON	
 #define AR_GAME_MENU_ICON_AXIS_X			(7)
-#define AR_GAME_MENU_ICON_AXIS_Y			(AR_GAME_MENU_FRAMES_AXIS_Y - 2)
-#define AR_GAME_MENU_ICON_SIZE_W			(15)
-#define AR_GAME_MENU_ICON_SIZE_H			(15)
-// Text
 #define AR_GAME_MENU_TEXT_AXIS_X			(20)
-#define AR_GAME_MENU_TEXT_AXIS_Y			(AR_GAME_MENU_ICON_AXIS_Y - 5)
 	// Scroll bar
 	view_render.fillRect(	scroll_bar.axis_x - 1, \
 							scroll_bar.axis_y, \
@@ -224,12 +188,36 @@ void view_scr_menu() {
 /* Handle - Menu game */
 /*****************************************************************************/
 void update_menu_screen_chosse() {
-	constrain(screen_menu.location, 1, NUMBER_MENU_ITEMS-1);
+	// Frames location
+	frame_white.axis_y =frame[screen_menu.location-screen_menu.screen].axis_y;
+	frame[0].axis_y = 0;
+	frame[1].axis_y = 22;
+	frame[2].axis_y = 44;
+	// update color menu
 	menu_chosse._id = 1<<screen_menu.location;
-	FRAMES_AXIS_Y();
 	MENU_ITEMS_ICON_COLOR();
+	// update scroll bar
 	scroll_bar.axis_y = (SCREEN_MENU_H*screen_menu.location / NUMBER_MENU_ITEMS);
-	constrain(screen_menu.screen, 1, NUMBER_MENU_ITEMS-3);	
+}
+
+void screen_tran_menu() {
+	switch (screen_menu.location) {
+	case 0:	// item 1
+		// SCREEN_TRAN(scr_archery_game_handle,	&scr_archery_game	);
+		break;
+	case 1:	// item 2
+		// SCREEN_TRAN(scr_archery_game_handle,	&scr_archery_game	);
+		break;
+	case 2:	// item 3
+		// SCREEN_TRAN(scr_archery_game_handle,	&scr_archery_game	);
+		break;
+	case 3: // item 4
+		// SCREEN_TRAN(scr_archery_game_handle,	&scr_archery_game	);
+		break;
+	
+	default:
+		break;
+	}
 }
 
 void scr_menu_handle(ak_msg_t* msg) {
@@ -244,23 +232,20 @@ void scr_menu_handle(ak_msg_t* msg) {
 
 	case AC_DISPLAY_BUTTON_MODE_RELEASED: {
 		APP_DBG_SIG("AC_DISPLAY_BUTTON_MODE_RELEASED\n");
-		switch (screen_menu.location) {
-		case 1:
-			/* code */
-			break;
-		
-		default:
-			break;
-		}
-
+		screen_tran_menu();
 	}
 		break;
 
 	case AC_DISPLAY_BUTTON_UP_RELEASED: {
 		APP_DBG_SIG("AC_DISPLAY_BUTTON_UP_RELEASED\n");
-		if (screen_menu.location > 0) {screen_menu.location--;}
+		// Menu [Up]
+		if (screen_menu.location > 0) {
+			screen_menu.location--;
+		}
 		if (frame_white.axis_y == frame[0].axis_y) {
-			if (screen_menu.screen > 0) {screen_menu.screen--;}
+			if (screen_menu.screen > 0) {
+				screen_menu.screen--;
+			}
 		}
 		else if (frame_white.axis_y == frame[1].axis_y) {
 			frame_white.axis_y = frame[0].axis_y;
@@ -274,7 +259,10 @@ void scr_menu_handle(ak_msg_t* msg) {
 
 	case AC_DISPLAY_BUTTON_DOWN_RELEASED: {
 		APP_DBG_SIG("AC_DISPLAY_BUTTON_DOWN_RELEASED\n");
-		if (screen_menu.location < NUMBER_MENU_ITEMS-1) {screen_menu.location++;}
+		// Menu [Down]
+		if (screen_menu.location < NUMBER_MENU_ITEMS-1) {
+			screen_menu.location++;
+		}
 		if (frame_white.axis_y == frame[0].axis_y) {
 			frame_white.axis_y = frame[1].axis_y;
 		}
@@ -282,7 +270,9 @@ void scr_menu_handle(ak_msg_t* msg) {
 			frame_white.axis_y = frame[2].axis_y;
 		}
 		else if (frame_white.axis_y == frame[2].axis_y) {
-			if (screen_menu.screen < NUMBER_MENU_ITEMS-3) {screen_menu.screen++;}
+			if (screen_menu.screen < NUMBER_MENU_ITEMS-3) {
+				screen_menu.screen++;
+			}
 		}
 		update_menu_screen_chosse();
 	}
